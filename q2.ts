2ba3c9eb94772e1ae5e5736a9d8904e9bcdb88c3
TeetatB <teetatmix36108@gmail.com>
2024-07-04 14:43:10 +0700

    finished

--- q2.ts
+++ q2.ts
@@ -1,9 +1,15 @@
-// define interface for Student object
-/* Your code here */
+// ชื่อ:ธีธัช บุญธรรม รหัสนักศึกษา:660610764
+interface Students{
+  name: string;
+  score: number;
+}
+
 
 // assign interface/type to the function definition properly
-function findTopNames(students) {
-  /* Your code here */
+function findTopNames(students: Students[]) {
+  let a:Students[] = students.filter((s:Students)=> s.score > 8);
+  let b:string[] = a.map((n:Students)=> n.name);
+  return b;
 }
 
 // assign interface/type to the student1 object properly
